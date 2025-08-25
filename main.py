715b1e5f241d61abd7f07073c79bb4ee5e8cacee
ggabrie-l <gabriel93blu@gmail.com>
2025-08-25 02:54:38 -0300

    AcheMeuFilme use para achar seus filmes próximos
    Terminado o app.

--- main.py
+++ main.py
@@ -7,30 +7,45 @@ headers = {
 
 def localizar():
     try:
-        response = requests.get("https://api.ipify.org?format=json")
-        response.raise_for_status()
-        endereco_ip = response.json()['ip']
+        resposta = requests.get("https://api.ipify.org?format=json")
+        resposta.raise_for_status()
+        endereco_ip = resposta.json()['ip']
 
-        response = requests.get(f"http://ip-api.com/json/{endereco_ip}")
-        response.raise_for_status()
-        localizacao = response.json()
+        resposta = requests.get(f"http://ip-api.com/json/{endereco_ip}")
+        resposta.raise_for_status()
+        localizacao = resposta.json()
 
         return localizacao
     except Exception as e:
         print(e)
 
+def filmes(ids: list):
+    shopping_id = ids[0]
+    city_id = ids[1]
+
+    resposta = requests.get(f"https://api-content.ingresso.com/v0/sessions/city/{city_id}/theater/{shopping_id}/partnership/null?date=2025-08-25", headers=headers)
+    resposta_json = resposta.json()
+
+    filmes_lista = resposta_json[0]['movies']
+
+    print(f"\t** Filmes para hoje  ( {resposta_json[0]['dayOfWeek']} ) ** \n")
+    for filme in range(len(filmes_lista)):
+        print(f"\t ~ {filmes_lista[filme]['title']}")
+
 if __name__ == "__main__":
     localizacao_usuario = localizar()
 
-    response = requests.get(f"https://api-content.ingresso.com/v0/theaters?partnership=nome_", headers=headers)
-    response2 = response.json()
+    resposta = requests.get(f"https://api-content.ingresso.com/v0/theaters?partnership=nome_", headers=headers)
+    resposta_json = resposta.json()
 
     city = localizacao_usuario['city']
     regionName = localizacao_usuario['regionName']
 
-    for d in response2:
-        if d['state'] == regionName and d['cityName'] == city:
-            print(f'Shopping : {d['name']}, em {d['neighborhood']} é o cinema mais perto de você!')
-            
+    for cinema in resposta_json:
+        if cinema['state'] == regionName and cinema['cityName'] == city:
+            print(f' *** Shopping : {cinema['name']}, em {cinema['neighborhood']} é o cinema mais perto de você! *** \n')
 
-        
+            ids = [cinema['id'], cinema['cityId']]
+
+            filmes(ids)
+         
